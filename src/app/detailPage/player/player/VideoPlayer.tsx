f52d8cfe7bc7209fb711f8585ad374f13779baa2
Abdukarim <abdukarimt463@gmail.com>
2026-04-27 00:45:15 +0500

    xatoliklar to'g'irlandi

--- src/app/detailPage/player/player/VideoPlayer.tsx
+++ src/app/detailPage/player/player/VideoPlayer.tsx
@@ -7,6 +7,7 @@ import {
     DialogTrigger,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button'; // Shadcn UI Button
+
 import { ArrowLeft } from 'lucide-react';
 
 // Agar FieldGroup va Field maxsus komponentlar bo'lsa, ularni ham import qiling. 
@@ -21,8 +22,16 @@ export default function VideoPlayer({ setPlay }: { setPlay: Dispatch<SetStateAct
                 </DialogTrigger>
             </Dialog>
             {/* Video Player Qismi */}
-            <iframe width="900" height="797" className='mx-auto mt-10 rounded-[20px] hover:-translate-y-1 duration-300 hover:scale-105 ' src="https://www.youtube.com/embed/4dr-k4NGp90" title="Honor 600 Lite - obzor (O’zbek tilida) " frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-
+            <iframe
+                width="900"
+                height="797"
+                className="mx-auto mt-10 rounded-[20px] hover:-translate-y-1 duration-300 hover:scale-105"
+                src="https://www.youtube.com/embed/4dr-k4NGp90"
+                title="Honor 600 Lite - obzor (O’zbek tilida)"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin" // Katta 'P' bilan
+                allowFullScreen // Katta 'FS' bilan va boolean sifatida
+            ></iframe>
             {/* Dialog Qismi */}
         </div>
     );
